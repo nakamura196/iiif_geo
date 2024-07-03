b1324f9e2749e07d322a26ee493a0a79b967436d
Satoru Nakamura <na.kamura.1263@gmail.com>
2024-07-03 13:37:38 +0900

    fix: bug

--- components/Help.vue
+++ components/Help.vue
@@ -5,9 +5,10 @@ const { locale } = useI18n();
 
 interface ContentData extends Record<string, any> {}
 
-const { data } = await useAsyncData<ContentData | null>("home", () =>
-  queryContent(`/help/${locale.value}`).findOne()
-);
+const { data } = await useAsyncData<ContentData | null>("home", async () => {
+  const result = await queryContent(`/help/${locale.value}`).findOne();
+  return result || {}; // Provide an empty object as a fallback
+});
 
 const dialog = ref(false);
 </script>
@@ -19,7 +20,7 @@ const dialog = ref(false);
     <v-dialog v-model="dialog">
       <v-card>
         <v-card-text>
-          <ContentRenderer class="nuxt-content" :value="data" />
+          <ContentRenderer v-if="data" class="nuxt-content" :value="data" />
         </v-card-text>
         <v-card-actions>
           <v-btn color="primary" block @click="dialog = false">{{
